7b96771dd256011733b27bf1431cf3c6e9133044
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2020-01-04 06:12:06 +0700

    Perubahan Nama Class Untuk Memudahkan Pengelompokkan Terhadap Rekursif Bercabang

--- src/com/rizkykhapidsyah/rekursif/RekursifBiasa.java
+++ src/com/rizkykhapidsyah/rekursif/RekursifBiasa.java
@@ -2,7 +2,7 @@ package com.rizkykhapidsyah.rekursif;
 
 import java.util.Scanner;
 
-public class Main {
+public class RekursifBiasa {
     public static void main(String[] args) {
         Scanner InputUSer = new Scanner(System.in);
 
